f0566e95903c4cce2b44918e8adc705b56fe0fd1
Garry Smith <ronintek0@gmail.com>
2016-01-24 16:56:52 -0500

    change output scss file to corral-frontend.css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    outputPaths: {
+        app: {
+          css: {
+            'app': '/assets/corral-frontend.css'
+          }
+        }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
