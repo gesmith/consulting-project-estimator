cd55be35cdcfd8dd39b7bb03843015ba1618aa55
Garry Smith <ronintek0@gmail.com>
2016-02-16 16:05:56 -0500

    add mirage route to config

--- mirage/config.js
+++ mirage/config.js
@@ -9,7 +9,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  //this.namespace = 'api';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
@@ -22,7 +22,5 @@ export default function() {
     this.del('/posts/:id');
   */
 
-  this.get('/api/estimates', (schema, request) => {
-    return schema.estimate.all();
-  });
+  this.get('/projects');
 }
