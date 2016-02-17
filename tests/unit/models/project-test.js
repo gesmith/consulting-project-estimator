ad1bd9d661e68d740e2f14132b339e550591f1fc
Garry Smith <ronintek0@gmail.com>
2016-02-16 23:03:06 -0500

    test project's relationship

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -1,12 +1,21 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('project', 'Unit | Model | project', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:estimate']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('should have many estimates', function(assert) {
+  const Project = this.store().modelFor('project');
+  const relationship = Ember.get(Project, 'relationshipsByName').get('estimates');
+
+  assert.equal(relationship.key, 'estimates', 'has relationship with estimate');
+  assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
