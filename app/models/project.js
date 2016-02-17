a8b3bb076815b81107c6ccaea59ad4ae18d93b86
Garry Smith <ronintek0@gmail.com>
2016-02-16 23:02:28 -0500

    pluralize estimates relationship prop

--- app/models/project.js
+++ app/models/project.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   created_at: DS.attr('date'),
-  estimate: DS.hasMany('estimate')
+  estimates: DS.hasMany('estimate')
 });
