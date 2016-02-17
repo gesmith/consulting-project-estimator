14549051b19d03b8fd9163779b48c0a19b165ee7
Garry Smith <ronintek0@gmail.com>
2016-02-16 22:03:44 -0500

    add properties and relationships to models

--- app/models/estimate.js
+++ app/models/estimate.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('string'),
+  created_at: DS.attr('date'),
+  assumptions: DS.attr('string'),
+  project: DS.belongsTo('project'),
+  features: DS.hasMany('feature')
 });
