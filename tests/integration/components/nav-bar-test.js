9187b9c27c04773487c9017602824084ff81fc8f
Garry Smith <ronintek0@gmail.com>
2016-02-18 19:17:31 -0500

    add test to test if navbar has icon

---
+++ tests/integration/components/nav-bar-test.js
@@ -0,0 +1,17 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('nav-bar', 'Integration | Component | nav bar', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{nav-bar}}`);
+
+  let projectsIconEntity = this.$().html('&#9776;').text();
+  assert.equal(this.$().text().trim(), projectsIconEntity);
+});
