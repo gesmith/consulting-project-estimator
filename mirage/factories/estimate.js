4d1c5ee8cd08b30a159cac6951f3b9f5edfcedb6
Garry Smith <ronintek0@gmail.com>
2016-02-24 18:19:10 -0500

    fix estimate factory

--- mirage/factories/estimate.js
+++ mirage/factories/estimate.js
@@ -1,7 +1,10 @@
 import { Factory } from 'ember-cli-mirage';
+import { faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
   name(i) { return `Estimate ${i}`; },
   created_at: new Date(),
-  assumptions: faker.lorem.paragraph()
+  assumptions(i) {
+    return faker.lorem.paragraph();
+  }
 });
