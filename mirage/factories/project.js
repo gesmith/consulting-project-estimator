63f818a377360dfc9213137f924d1b55d241ed49
Garry Smith <ronintek0@gmail.com>
2016-02-24 18:19:24 -0500

    add int to project factory

--- mirage/factories/project.js
+++ mirage/factories/project.js
@@ -2,7 +2,7 @@ import { Factory } from 'ember-cli-mirage';
 import { faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  name() {
+  name(i) {
     return faker.company.companyName();
   },
   created_at: new Date()
